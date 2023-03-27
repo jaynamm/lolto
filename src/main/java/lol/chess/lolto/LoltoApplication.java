a0be9c796a5dad05f986dd1fd291abb93ca109ca
jaynamm <polky0421@gmail.com>
2023-03-27 20:54:56 +0900

    Create project

---
+++ src/main/java/lol/chess/lolto/LoltoApplication.java
@@ -0,0 +1,13 @@
+package lol.chess.lolto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoltoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoltoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/lol/chess/lolto/LoltoApplicationTests.java
@@ -0,0 +1,13 @@
+package lol.chess.lolto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoltoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
